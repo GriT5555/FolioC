dd442252b008b7b3bb50354371b038456a55344b
Mat <113144372+GriT5555@users.noreply.github.com>
2026-08-21 12:08:14 +0200

    2nd container and scss
    Scss update and second container for ongoing projects

--- src/components/CardsContainer.jsx
+++ src/components/CardsContainer.jsx
@@ -1,12 +1,13 @@
 import Cards from "../Subcomps/Cards";
 import Data from "../Projects.json"
+import Data2 from "../wip.json"
 
-export default function CardsContainer({title}){
+export default function CardsContainer(){
 
     return(
         <section className="Projects">
             <section /*Finis*/className="ProjectCards">
-                <h1 className="Conth2">{title}</h1>
+                <h1 className="Conth2">Builds terminés</h1>
                 <div className="Cardbox">
                     {Data.map((project) => {
                         let stackcontent;
@@ -23,6 +24,31 @@ export default function CardsContainer({title}){
                                    title={project.title}
                                    cover={project.cover}
                                    stack={project.stack}
+                            />
+
+                        )
+                    })}
+                </div>
+            </section>
+
+            <section /*Finis*/className="ProjectCards">
+                <h1 className="Conth2">WIP builds</h1>
+                <div className="Cardbox">
+                    {Data2.map((project2) => {
+                        let stackcontent;
+                        if(Array.isArray(project2.stack)) {
+                            stackcontent = project2.stack.map((tech, i) => <li key={i}>{tech}</li>);
+                        }   else if(project2.stack) {
+                            stackcontent = <li>{project2.stack}</li>;
+                        }   else if(project2.stack) {
+                            stackcontent = <li>No stack listed</li>
+                        }
+                        
+                        return(
+                            <Cards key={project2.id} 
+                                   title={project2.title}
+                                   cover={project2.cover}
+                                   stack={project2.stack}
                             />
 
                         )
